4bc2fc09b75f47861a057fcebedb44aef153c58a
sedhu24 <smilesedhu001@gmail.com>
2023-01-06 14:01:10 +0530

    First

---
+++ lib/app/env.dart
@@ -0,0 +1,8 @@
+class Environments {
+  static const String appName = 'Food Duke';
+  static const String companyName = 'Byot  Technology';
+  static const String apiBaseURL = 'http://api.foodduke.com/';
+  static const String websiteURL = 'https://foodies.bunchdevelopers.com/';
+
+  static const String googleMapsKey = 'YOUR-GOOGLE-MAPS-KEYS';
+}
